a4f9658a5547e5c24a539a54ef6c47715e0476c4
Andrew Norris <ahn288@nyu.edu>
2020-05-17 16:00:26 -0400

    add time tracker

--- BDM_FinalChallenge.py
+++ BDM_FinalChallenge.py
@@ -115,9 +115,9 @@ def violations_per_streetline(output_folder):
 
 if __name__ == '__main__':
 	output_folder = sys.argv[1]
-	starttime = datetime.dt.now()
+	starttime = dt.datetime.now()
 	run_spark(str(p.output_path))
-	elapsed = datetime.dt.now() - starttime
+	elapsed = dt.datetime.now() - starttime
 	print("Done, Elapsed: {} (secs)".format(elapsed.total_seconds()))
 	violations_per_streetline(output_folder) 
 
